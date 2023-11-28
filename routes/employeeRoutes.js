400b830ad2f4a7d9796959fe52a26f42dd316cc5
Ayobamee <ayobami.elutade@com>
2023-11-28 07:44:27 +0100

    Fixed the broken delete endpoint

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -37,13 +37,20 @@ router.put("/", async (req, res) => {
   }
 });
 
-router.delete("/", async (req, res) => {
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(`Attempting to delete employee with ID: ${id}`); // Log the ID
+
   try {
-    const deletedEmployee = await Employee.findByIdAndDelete(req.params.id);
-    if (!deletedEmployee)
+    const employee = await Employee.findByIdAndDelete(id);
+    if (!employee) {
+      console.log(`No employee found with ID: ${id}`); // Log if not found
       return res.status(404).json({ message: "Employee not found" });
-    res.json({ message: "Employee deleted successfully" });
+    }
+    console.log(`Deleted employee with ID: ${id}`); // Log if deletion was successful
+    res.json({ message: "Employee deleted successfully", employee });
   } catch (err) {
+    console.error(err); // Log any errors
     res.status(500).json({ message: err.message });
   }
 });
